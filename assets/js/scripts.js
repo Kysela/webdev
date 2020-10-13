b87f26570890b6d62f230d30888a25c705d17183
Kysela <bythedroid@gmail.com>
2020-10-13 20:53:51 +0200

    remove trash

--- assets/js/scripts.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-   if(document.body.style.background == "black"){
-      document.body.style.background = "white";
-   } else{
-      document.body.style.background = "black";
-   }
-}
